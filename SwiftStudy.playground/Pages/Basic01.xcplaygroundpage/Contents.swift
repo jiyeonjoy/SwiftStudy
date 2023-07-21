495a0541ca994dd9bfb6f03a49b234aaf02923b6
최지연 <choi02647@naver.com>
2023-07-21 14:30:36 +0900

    struct 정리

--- SwiftStudy.playground/Pages/Basic01.xcplaygroundpage/Contents.swift
+++ SwiftStudy.playground/Pages/Basic01.xcplaygroundpage/Contents.swift
@@ -1,5 +1,93 @@
 import Foundation
 
+// -------------------------------------------------//
+//struct vs. class 차이점과 공통점
+//
+//✅ 공통점
+//- 프로퍼티(속성)를 정의해서 값을 저장할수 있음
+//- 메소드(행동)를 정의해서 기능을 제공할수 있음
+//- 생성자를 정의해서 초기상태를 세팅할수 있음
+//- 확장을 이용해서, 기본 구현외 추가 기능을 더할수 있음
+//- 프로토콜을 구현해서 특정 기능을 제공할수 있음
+//
+//✅ 차이점
+//- 클래스는 상속을 시킬수 있음
+//- 클래스는 레퍼런스 카운팅을 통해 클래스 인스턴스에 대한 하나 이상의 참조를 허용
+//- 클래스는 deinitializer 호출시, 이미 할당된 리소스에서 해제 할수 있음
+
+
+// -------------------------------------------------//
+// property
+struct Movie {
+    // stored property
+    var name: String
+    var director: String
+
+    // computed property
+    var description: String {
+        return "\(name) is \(director)'s best movie ever"
+    }
+}
+
+var movie = Movie(name: "인셉션", director: "놀란")
+print(movie.description)
+
+
+// -------------------------------------------------//
+// didSet
+struct Task {
+    var name: String
+    var progress: Int {
+        didSet {
+            print("Current Progress : \(progress) %")
+        }
+    }
+    
+    var isDone: Bool {
+        return progress == 100
+    }
+}
+
+var task = Task(name: "Very Important Task", progress: 0)
+
+task.progress = 30 // Current Progress : 30 %
+task.progress = 50 // Current Progress : 50 %
+task.progress = 90 // Current Progress : 90 %
+
+
+// -------------------------------------------------//
+//# method
+//
+//- 객체는 행동(기능)을 가지고 있고, 그것을 우리는 `method` 라고 부름
+//    - 객체가 가지고 있는 함수라고 생각하면 됨
+//
+//- `struct` 에서 `method` 가 `stored property` 값을 변경하는 경우, `mutating` 키워드를 메소드 앞에 붙여주어야함
+
+
+// -------------------------------------------------//
+// mutating
+struct Student {
+    let name: String
+    var major: String
+    var knowledge: Double
+    
+    func didFinalTest() -> Int {
+        let howMuchIdontKnow = (1 - knowledge) * 100
+        let score = 100 - Int(howMuchIdontKnow)
+        return score
+    }
+    
+    mutating func didStudy() {
+        if knowledge >= 1 {
+            knowledge = 1
+        } else {
+            knowledge += 0.1
+        }
+    }
+}
+
+
+// -------------------------------------------------//
 // 03
 // inout
 func makeTriple(num: inout Int) {
@@ -12,6 +100,7 @@ makeTriple(num: &num)
 print(num) // 24
 
 
+// -------------------------------------------------//
 // 02
 // throws
 enum DivideError: Error {
@@ -35,6 +124,7 @@ do {
 }
 
 
+// -------------------------------------------------//
 // 01
 // print
 func printNames(_ names: String...) {
