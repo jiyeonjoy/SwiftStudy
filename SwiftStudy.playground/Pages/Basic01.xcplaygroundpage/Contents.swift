5ae33e709b952ff22a6c7e54df660f42c9ba0ad2
최지연 <choi02647@naver.com>
2023-07-21 14:18:06 +0900

    test print

--- SwiftStudy.playground/Pages/Basic01.xcplaygroundpage/Contents.swift
+++ SwiftStudy.playground/Pages/Basic01.xcplaygroundpage/Contents.swift
@@ -1,4 +1,14 @@
 import Foundation
 
-var greeting = "Hello, playground"
+func printNames(_ names: String...) {
+    for name in names {
+        print("name is \(name)")
+    }
+}
 
+printNames("James", "Roy", "Jake")
+// name is James
+// name is Roy
+// name is Jake
+
+print("hi", 1, false)
