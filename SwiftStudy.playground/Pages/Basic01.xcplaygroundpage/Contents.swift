6ba3096042a17691f9fd4c4def44d2bb4d1a64dc
최지연 <choi02647@naver.com>
2023-07-21 14:21:07 +0900

    02 - throws

--- SwiftStudy.playground/Pages/Basic01.xcplaygroundpage/Contents.swift
+++ SwiftStudy.playground/Pages/Basic01.xcplaygroundpage/Contents.swift
@@ -1,5 +1,30 @@
 import Foundation
 
+// 02
+// throws
+enum DivideError: Error {
+    case cannotZero
+}
+
+func divide(dividend: Int, divisor: Int) throws -> Int {
+    if divisor == 0 {
+        throw DivideError.cannotZero
+    }
+    
+    return Int(dividend / divisor)
+}
+
+do {
+    let result = try divide(dividend: 80, divisor: 6)
+//    let result = try divide(dividend: 80, divisor: 0)
+    print(result)
+} catch {
+    print(error.localizedDescription)
+}
+
+
+// 01
+// print
 func printNames(_ names: String...) {
     for name in names {
         print("name is \(name)")
